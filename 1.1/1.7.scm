17da0cb3936d94d76a95b2758ca25c3b71511fd7
Sayem Islam <sayem.islam@gmail.com>
2011-03-16 18:11:30 -0400

    1.7-.18

--- 1.1/1.7.scm
+++ 1.1/1.7.scm
@@ -20,6 +20,11 @@
   (sqrt-iter 1.0 x))
 
 
-; 
+; It's apparent that the test fails for very small numbers. As an example, the square root of 0.0003 is 0.0173. However, when done using the good-enough? test above in the sqrt procedure you get 0.0344, which is a significant difference from the actual answer. The reason this happens is simply because of the 0.001 tolerance that's set in the good-enough? test.
 
+; For very large numbers, such as 30 trillion, good-enough? test also fails. The square root of 30 trillion is 17,320,508, but when attempted with the sqrt procedure the result is never computed as there's limited precision between the large numbers and the program just crashes. 
 
+(define (good-enough? guess x)
+  (< (abs (- (improve guess x) guess)) (* guess .001))) 
+
+; Modifying the good-enough? test so that it's a relative tolerance (* guess 0.001) accounts for extremities in scale and works better for both very large and very small numbers. 
---
+++ 1.1/1.8.scm
@@ -0,0 +1,21 @@
+
+(define (good-enough? guess x)
+  (< (abs (- (improve guess x) guess)) (* guess .001))) 
+
+(define (improve guess x)
+  (/ (+ (/ x (* guess guess)) (* 2 guess)) 3))
+
+(define (average x y)
+  (/ (+ x y) 2))
+
+(define (cubert-iter guess x)
+  (if (good-enough? guess x)
+      guess
+      (cubert-iter (improve guess x)
+		 x)))
+
+(define (cubert x)
+  (cubert-iter 1.0 x))
+
+
+; This procedure calculates the cube root of x using Newton's method for getting better approximations of y with: (x/y^2 + 2y)/3
