e8cb26ddcb84bb81d3c46a4b3ed07af4004f3ada
Sayem Islam <sayem.islam@gmail.com>
2011-03-16 21:28:55 -0400

    1.2.1

--- 1.2/1.10.scm
+++ 1.2/1.10.scm
@@ -1,2 +1,27 @@
 
-;
+; Ackermann's function (http://en.wikipedia.org/wiki/Ackermann_function):
+
+(define (A x y)
+  (cond ((= y 0) 0)
+	((= x 0) (* 2 y))
+	((= y 1) 2)
+	(else (A (- x 1)
+		 (A x (- y 1))))))
+
+
+(A 1 10) = 1,024
+
+(A 2 4) = 65,536
+
+(A 3 3) = 65,536
+
+
+(define (f n) (A 0 n)) = 2n
+
+(define (g n) (A 1 n)) = 2^n
+
+(define (h n) (A 2 n)) = 2^2^... (n times)
+
+(define (f n) (* 5 n n)) = 5n^2
+
+
