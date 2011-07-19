b58a92879b1243917745c048a945f1159affe940
Sayem Islam <sayem.islam@gmail.com>
2011-07-19 17:39:57 -0400

    1.16

--- 1.2/1.16.scm
+++ 1.2/1.16.scm
@@ -1,16 +1,17 @@
 
-(define (expt b n)
-  (expt-iter b n 1))
-
-(define (expt-iter b counter product)
-  (if (= counter 0)
-      product
-      (expt-iter b
-		 (- counter 1)
-		 (* b product))))
-
 (define (even? n)
   (= (remainder n 2) 0))
 
+(define (square x)
+  (* x x))
 
-; need to keep iterating this for product for b^n-1 for odd and 
+
+; ab^n is unchanged from state to state:
+
+(define (expt b n)
+  (expt-iter b n 1))
+
+(define (expt-iter b n a)
+  (cond ((= n 0) a)
+	((even? n) (expt-iter (square b) (/ n 2) a))
+	(else (expt-iter b (- n 1) (* a b)))))
