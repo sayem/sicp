bb9421dbb69b767c0107162e451dde2b6ddb2e57
Sayem Islam <sayem.islam@gmail.com>
2012-03-27 00:21:02 -0700

    1.24 fermat test

--- 1.2/1.24.scm
+++ 1.2/1.24.scm
@@ -1,1 +1,39 @@
 
+(define (timed-prime-test n)
+  (newline)
+  (display n)
+  (start-prime-test n (current-inexact-milliseconds)))
+
+(define (start-prime-test n start-time)
+  (if (fast-prime? n 100) 
+      (report-prime (- (current-inexact-milliseconds) start-time))
+      (newline)))
+
+(define (report-prime elapsed-time)
+  (display " *** ")
+  (display elapsed-time)
+  (newline))
+
+(define (square a) (* a a))
+
+(define (even? n)
+   (= (remainder n 2) 0))
+
+(define (expmod base exp m)
+  (cond ((= exp 0) 1)
+        ((even? exp)
+         (remainder (square (expmod base (/ exp 2) m))
+                    m))
+        (else
+         (remainder (* base (expmod base (- exp 1) m))
+                    m))))        
+
+(define (fermat-test n)
+  (define (try-it a)
+    (= (expmod a n n) a))
+  (try-it (+ 1 (random (- n 1)))))
+
+(define (fast-prime? n times)
+  (cond ((= times 0) true)
+        ((fermat-test n) (fast-prime? n (- times 1)))
+        (else false)))
