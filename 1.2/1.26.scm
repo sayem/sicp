04222aa3a443dd21ff1eba9d80e0714fca178c28
Sayem Islam <sayem.islam@gmail.com>
2012-03-29 23:18:04 -0700

    1.26 - 1.27

--- 1.2/1.26.scm
+++ 1.2/1.26.scm
@@ -1,1 +1,12 @@
 
+(define (expmod base exp m)
+  (cond ((= exp 0) 1)
+        ((even? exp)
+         (remainder (* (expmod base (/ exp 2) m)
+                       (expmod base (/ exp 2) m))
+                    m))
+        (else
+         (remainder (* base (expmod base (- exp 1) m))
+                    m))))
+
+; Using explicit multiplication instead of square results in much slower performance. Reason being that (expmod base (/ exp 2) m) is called twice as many times, and consequently, what was originally a linear recursive process has turned into a tree recursion. Each (expmod base (/ exp 2) m) branches out to more and more child processes, and so with explicit multiplication we have O(log 2^n), which is O(n).
--- 1.2/1.27.scm
+++ 1.2/1.27.scm
@@ -1,1 +1,39 @@
 
+(define (timed-prime-test n)
+  (newline)
+  (display n)
+  (start-prime-test n (current-inexact-milliseconds)))
+
+(define (start-prime-test n start-time)
+  (if (fast-prime? (- n 1) n) 
+      (report-prime (- (current-inexact-milliseconds) start-time))
+      (newline)))
+
+(define (report-prime elapsed-time)
+  (display " *** ")
+  (display elapsed-time)
+  (newline))
+
+(define (square a) (* a a))
+
+(define (even? n)
+   (= (remainder n 2) 0))
+
+(define (expmod base exp m)
+  (cond ((= exp 0) 1)
+        ((even? exp)
+         (remainder (square (expmod base (/ exp 2) m))
+                    m))
+        (else
+         (remainder (* base (expmod base (- exp 1) m))
+                    m))))
+
+(define (fermat-test n)
+  (define (try-it a)
+    (= (expmod a n n) a))
+  (try-it (+ 1 (random (- n 1)))))
+
+(define (fast-prime? a n)
+  (cond ((= a 0) true)
+        ((= (expmod a n n) a) fast-prime? (- a 1) n)
+        (else false)))
