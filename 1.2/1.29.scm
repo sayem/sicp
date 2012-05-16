00f6f4bdc76d21fff8ef533ab4ac790c72630f2e
Sayem Islam <sayem.islam@gmail.com>
2012-05-16 00:39:51 -0700

    1.29 update

--- 1.2/1.29.scm
+++ 1.2/1.29.scm
@@ -1,1 +1,20 @@
 
+(define (sum term a next b)
+  (if (> a b)
+      0
+      (+ (term a)
+         (sum term (next a) next b))))
+
+(define (simpson-integral f a b n)
+  (define h (/ (- b a) n))
+  (define (inc x) (+ x 1))
+  (define (y k)
+    (f (+ a (* k h))))
+  (define (term k)
+    (* (cond ((odd? k) 4)
+             ((or (= k 0) (= k n)) 1)
+             ((even? k) 2))
+       (y k)))
+  (/ (* h (sum term 0 inc n)) 3))
+
+(define (cube x) (* x x x))
